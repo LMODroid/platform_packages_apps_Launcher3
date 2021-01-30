640fd48408a2bf69e0b7625c7b73dec306421565
Ali B <abittin@gmail.com>
2021-01-30 22:36:40 +0300

    Launcher3: Add uninstall button to system shortcuts
    [ghostrider-reborn: Adapt to Sv2, 13]
    
    Squashed:
    
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Sun Jun 26 18:44:18 2022 +0530
    
        Launcher3: SystemShortcut: Avoid NPE due to uninstall button
    
        Crash log: https://bin.kv2.dev/~62b85c7008bb984d9dc61e94
    
        Steps to reproduce:
        - Add Now Playing widget to home screen
        - Long press on it
        - Launcher crashes
    
        Test: manual; no longer crashes
        Change-Id: I6c0f73e38a3695a64d820c40b58f5e87ba7013c5
    
    [adithya2306: Use existing uninstall shortcut in 15-qpr0]
    
    Change-Id: I005d676d9a98f65296c330e5e13fd0d849df6fe5
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -469,9 +469,7 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
         if (Flags.enableShortcutDontSuggestApp()) {
             shortcuts.add(DONT_SUGGEST_APP);
         }
-        if (Flags.enablePrivateSpace()) {
-            shortcuts.add(UNINSTALL_APP);
-        }
+        shortcuts.add(UNINSTALL_APP);
         if (enableBubbleAnything()) {
             shortcuts.add(BUBBLE_SHORTCUT);
         }
