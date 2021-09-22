2222eccdb8c1471720f9199bc7b2eec0449f25cf
Trishiraj <gamerprince.exp@gmail.com>
2021-09-23 03:34:14 +0530

    Launcher3: Add toggle for translucent hotseat background
    @neobuddy89: Disabled by default.
    
    Squashed below commits:
        From: Dil3mm4 <dil3mm4.dev@gmail.com>
        Date: Wed, 30 Dec 2020 20:38:39 +0000
        Subject: Appseat: add a bkg
    
        From: StarkDroid <gamerprince.exp@gmail.com>
        Date: Tue, 31 May 2022 16:16:56 -0500
        Subject: Hotseat background: Corner radius only at the top
    
        Rounding off at all sides makes it look like its not attached to anything
    
        From: minaripenguin <alexfinhart@gmail.com>
        Date: Fri, 22 Jul 2022 05:49:42 +0800
        Subject: Launcher3: Link hotseat bg corner radius to dialogCornerRadius attribute
    
    Change-Id: I7333e3fbe5e73188920f09c81304b3f6e8a5fc3e
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -157,6 +157,7 @@ public final class Utilities {
 
     public static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
     public static final String KEY_DOCK_SEARCH = "pref_dock_search";
+    public static final String KEY_SHOW_HOTSEAT_BG = "pref_show_hotseat_bg";
     public static final String LENS_ACTIVITY = "com.google.android.apps.lens.MainActivity";
     public static final String LENS_URI = "google://lens";
 
@@ -979,4 +980,8 @@ public final class Utilities {
         return prefs.getBoolean(KEY_DOCK_SEARCH, true);
     }
 
+    public static boolean isHotseatBgEnabled(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_SHOW_HOTSEAT_BG, false);
+    }
 }
