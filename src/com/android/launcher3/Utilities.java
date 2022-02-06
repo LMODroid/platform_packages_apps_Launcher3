585c4810a947adc06b8e642211d1c6ed4fdf4a2a
Pranav Vashi <neobuddy89@gmail.com>
2022-02-07 01:50:09 +0530

    Launcher3: Enable lens icon for dock searchbar
    Change-Id: I5373a4cefed9048f746c87f3ccafb48adf8456ed
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -157,6 +157,8 @@ public final class Utilities {
 
     public static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
     public static final String KEY_DOCK_SEARCH = "pref_dock_search";
+    public static final String LENS_ACTIVITY = "com.google.android.apps.lens.MainActivity";
+    public static final String LENS_URI = "google://lens";
 
     /**
      * Returns true if theme is dark.
