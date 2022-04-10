fc3f196bf815641998349e79de98d1a16282f601
jhonboy121 <alfredmathew05@gmail.com>
2022-04-11 02:00:42 +0530

    Launcher3: add support for custom themed icon pack [3/3]
    rdEdit: forward port to 13, misc code revisions and adaptation for custom icon packs, reverted try-with-resources changes from 68af549a459aef69084ee39b0bbbdfda5cf87dcc
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Author: minaripenguin <minaripenguin@users.noreply.github.com>
    Date:   Thu Dec 1 17:11:40 2022 +0800
    
        fixup! Launcher3: Add custom themed icons support
    
        * Follow AOSP code changes for themed icons
        * overwrite previous themed icons arraymap since users will most like change themed icon packs
    
        test: rename 3 custom themed icon packs to different packagenames, install, switch themed icon packs including default (system icons)
    
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Author: Rizky Benggolo <travarilo@gmail.com>
    Date:   Sun Dec 18 08:24:11 2022 +0000
    
        Launcher3: Adapt custom themed icons to QPR1
    
    Author: minaripenguin <minaripenguin@users.noreply.github.com>
    Date:   Tue July 4th 11:47:21 2023 +0000
    
        Launcher3: Adapt custom themed icons to QPR3
    
    Change-Id: Ieb57159cae2ae871af7add56ef2c308e285cd63a
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: Farzin <itisFarzin@gmail.com>

--- src/com/android/launcher3/icons/IconCache.java
+++ src/com/android/launcher3/icons/IconCache.java
@@ -70,6 +70,7 @@ import com.android.launcher3.shortcuts.ShortcutKey;
 import com.android.launcher3.util.CancellableTask;
 import com.android.launcher3.util.InstantAppResolver;
 import com.android.launcher3.util.PackageUserKey;
+import com.android.launcher3.util.Themes;
 import com.android.launcher3.widget.WidgetSections;
 import com.android.launcher3.widget.WidgetSections.WidgetSection;
 
@@ -105,6 +106,7 @@ public class IconCache extends BaseIconCache {
     private final InstantAppResolver mInstantAppResolver;
     private final IconProvider mIconProvider;
     private final CancellableTask mCancelledTask;
+    private final String mCustomThemedIconPack;
 
     private final SparseArray<BitmapInfo> mWidgetCategoryBitmapInfos;
 
@@ -125,6 +127,7 @@ public class IconCache extends BaseIconCache {
 
         mCancelledTask = new CancellableTask(() -> null, MAIN_EXECUTOR, c -> { });
         mCancelledTask.cancel();
+        mCustomThemedIconPack = Themes.getThemedIconPack(context);
     }
 
     @Override
@@ -622,7 +625,7 @@ public class IconCache extends BaseIconCache {
     }
 
     public Drawable getFullResIcon(LauncherActivityInfo info) {
-        return mIconProvider.getIcon(info, mIconDpi);
+        return mIconProvider.getIcon(info, mIconDpi, mCustomThemedIconPack);
     }
 
     public void updateSessionCache(PackageUserKey key, PackageInstaller.SessionInfo info) {
