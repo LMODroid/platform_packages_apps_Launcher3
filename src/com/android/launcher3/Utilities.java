35b3932a94497727495ee0451d50f37d89373997
Nauval Rizky <enuma.alrizky@gmail.com>
2022-07-03 01:01:53 +0000

    Launcher3: Add option to control wallpaper zooming
    @neobuddy89: Updated for A13 QPR1
    
    Change-Id: I11f297ae02831b30ab731c008c653fc65200a7b8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -156,6 +156,7 @@ public final class Utilities {
     public @interface AdjustmentDirection{}
 
     public static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
+    public static final String KEY_ALLOW_WALLPAPER_ZOOMING = "pref_allow_wallpaper_zooming";
     public static final String KEY_DOCK_SEARCH = "pref_dock_search";
     public static final String KEY_SHOW_HOTSEAT_BG = "pref_show_hotseat_bg";
     public static final String LENS_ACTIVITY = "com.google.android.apps.lens.MainActivity";
@@ -983,5 +984,10 @@ public final class Utilities {
     public static boolean isHotseatBgEnabled(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_SHOW_HOTSEAT_BG, false);
+    }
+
+    public static boolean canZoomWallpaper(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_ALLOW_WALLPAPER_ZOOMING, true);
     }
 }
