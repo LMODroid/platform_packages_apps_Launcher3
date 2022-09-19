LineageOS/android_packages_apps_Trebuchet

Add support for toggling taskbar
- Use new API to enable/disable SysUI taskbar integration
- User-facing setting to control taskbar eligibility
- Reload by restarting (live reload would be much more complicated)
- Update SysUI integration state before reloading

Depends on frameworks/base commit:
    SystemUI: Add API for runtime taskbar config

Change-Id: Id1889ca9b102f94cc0a1161f49de88428dcc0943

src/com/android/launcher3/InvariantDeviceProfile.java
@@ -25,6 +25,7 @@ import static com.android.launcher3.util.Executors.MAIN_EXECUTOR;
 import android.annotation.TargetApi;
 import android.appwidget.AppWidgetHostView;
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -32,9 +33,13 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.content.res.XmlResourceParser;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
@@ -56,6 +61,9 @@ import com.android.launcher3.util.IntArray;
 import com.android.launcher3.util.MainThreadInitializedObject;
 import com.android.launcher3.util.Themes;
 import com.android.launcher3.util.WindowBounds;
+import com.android.quickstep.SystemUiProxy;
+
+import lineageos.providers.LineageSettings;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -176,6 +184,31 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
 
     private final ArrayList<OnIDPChangeListener> mChangeListeners = new ArrayList<>();
 
+    private static final Uri ENABLE_TASKBAR_URI = LineageSettings.System.getUriFor(
+            LineageSettings.System.ENABLE_TASKBAR);
+
+    private final class SettingsContentObserver extends ContentObserver {
+        SettingsContentObserver() {
+            super(new Handler(Looper.getMainLooper()));
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (ENABLE_TASKBAR_URI.equals(uri)) {
+                // Create the illusion of this taking effect immediately
+                // Also needed because TaskbarManager inits before SystemUiProxy on start
+                boolean enabled = LineageSettings.System.getInt(mContext.getContentResolver(),
+                        LineageSettings.System.ENABLE_TASKBAR, 0) == 1;
+                SystemUiProxy.INSTANCE.get(mContext).setTaskbarEnabled(enabled);
+
+                // Restart launcher
+                System.exit(0);
+            }
+        }
+    }
+
+    private final SettingsContentObserver mSettingsObserver = new SettingsContentObserver();
+
     @VisibleForTesting
     public InvariantDeviceProfile() {
     }
@@ -199,6 +232,10 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
                         onConfigChanged(displayContext);
                     }
                 });
+
+        final ContentResolver resolver = mContext.getContentResolver();
+        resolver.registerContentObserver(ENABLE_TASKBAR_URI, false,
+                mSettingsObserver);
     }
 
     /**

LMODroid/platform_packages_apps_Launcher3

src/com/android/launcher3/InvariantDeviceProfile.java
@@ -25,6 +25,7 @@ import static com.android.launcher3.util.Executors.MAIN_EXECUTOR;
 import android.annotation.TargetApi;
 import android.appwidget.AppWidgetHostView;
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -32,9 +33,14 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.content.res.XmlResourceParser;
+import android.database.ContentObserver;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
@@ -56,6 +62,7 @@ import com.android.launcher3.util.IntArray;
 import com.android.launcher3.util.MainThreadInitializedObject;
 import com.android.launcher3.util.Themes;
 import com.android.launcher3.util.WindowBounds;
+import com.android.quickstep.SystemUiProxy;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -176,6 +183,31 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
 
     private final ArrayList<OnIDPChangeListener> mChangeListeners = new ArrayList<>();
 
+    private static final Uri ENABLE_TASKBAR_URI = Settings.System.getUriFor(
+            Settings.System.ENABLE_TASKBAR);
+
+    private final class SettingsContentObserver extends ContentObserver {
+        SettingsContentObserver() {
+            super(new Handler(Looper.getMainLooper()));
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (ENABLE_TASKBAR_URI.equals(uri)) {
+                // Create the illusion of this taking effect immediately
+                // Also needed because TaskbarManager inits before SystemUiProxy on start
+                boolean enabled = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.ENABLE_TASKBAR, 0) == 1;
+                SystemUiProxy.INSTANCE.get(mContext).setTaskbarEnabled(enabled);
+
+                // Restart launcher
+                System.exit(0);
+            }
+        }
+    }
+
+    private final SettingsContentObserver mSettingsObserver = new SettingsContentObserver();
+
     @VisibleForTesting
     public InvariantDeviceProfile() {
     }
@@ -199,6 +231,10 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
                         onConfigChanged(displayContext);
                     }
                 });
+
+        final ContentResolver resolver = mContext.getContentResolver();
+        resolver.registerContentObserver(ENABLE_TASKBAR_URI, false,
+                mSettingsObserver);
     }
 
     /**