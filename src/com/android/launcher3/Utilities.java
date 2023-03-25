b1085d3fd7d6357ae37b66b96dc941db9891652e
nift4 <nift4@protonmail.com>
2023-03-25 21:06:14 +0100

    fixup! Launcher3: Add toggle for DT2S
    Change-Id: I92ee50fdf2006d9a07b99e3bd91e07e3c6aa6940
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -752,7 +752,7 @@ public final class Utilities {
     }
 
     public static boolean isDoubleTapGestureEnabled(Context context) {
-        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DT_GESTURE, true);
     }
 
