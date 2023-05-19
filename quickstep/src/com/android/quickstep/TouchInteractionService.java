f0efd73cd2093ce4ed526c0c0988c2f2b127bd4d
nift4 <nift4@protonmail.com>
2023-05-19 16:05:04 +0200

    Launcher3: support navbar surface drag-n-drop [2/2]
    Change-Id: I434adbde4f2d763c71e7a6d0766207144006c85c
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- quickstep/src/com/android/quickstep/TouchInteractionService.java
+++ quickstep/src/com/android/quickstep/TouchInteractionService.java
@@ -47,6 +47,7 @@ import static com.android.systemui.shared.system.ActivityManagerWrapper.CLOSE_SY
 import static com.android.systemui.shared.system.QuickStepContract.KEY_EXTRA_SYSUI_PROXY;
 import static com.android.systemui.shared.system.QuickStepContract.KEY_EXTRA_UNFOLD_ANIMATION_FORWARDER;
 import static com.android.systemui.shared.system.QuickStepContract.KEY_EXTRA_UNLOCK_ANIMATION_CONTROLLER;
+import static com.android.systemui.shared.system.QuickStepContract.SYSUI_STATE_SCREEN_PINNING;
 import static com.android.wm.shell.Flags.enableBubblesLongPressNavHandle;
 import static com.android.wm.shell.sysui.ShellSharedConstants.KEY_EXTRA_SHELL_BACK_ANIMATION;
 import static com.android.wm.shell.sysui.ShellSharedConstants.KEY_EXTRA_SHELL_BUBBLES;
@@ -571,6 +572,8 @@ public class TouchInteractionService extends Service {
     private InputManager mInputManager;
     private final Set<Integer> mTrackpadsConnected = new ArraySet<>();
 
+    private boolean mCancelGesture = false;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -721,6 +724,17 @@ public class TouchInteractionService extends Service {
             mOverviewComponentObserver.onSystemUiStateChanged();
             mTaskbarManager.onSystemUiFlagsChanged(systemUiStateFlags);
             mTaskAnimationManager.onSystemUiFlagsChanged(lastSysUIFlags, systemUiStateFlags);
+
+            if ((lastSysUIFlags & SYSUI_STATE_SCREEN_PINNING) !=
+                (systemUiStateFlags & SYSUI_STATE_SCREEN_PINNING)) {
+                if ((systemUiStateFlags & SYSUI_STATE_SCREEN_PINNING) != 0) {
+                    // cancel in-progress gesture, if any. users must not reach recents
+                    mCancelGesture = true;
+                } else {
+                    // If user has tried no gesture since blocking the gesture, we need to reset this
+                    mCancelGesture = false;
+                }
+            }
         }
     }
 
@@ -919,8 +933,10 @@ public class TouchInteractionService extends Service {
             }
         }
 
-        boolean cancelGesture = mGestureState.getContainerInterface() != null
-                && mGestureState.getContainerInterface().shouldCancelCurrentGesture();
+        boolean myCancel = mCancelGesture;
+        if (myCancel) mCancelGesture = false;
+        boolean cancelGesture = (mGestureState.getContainerInterface() != null
+                && mGestureState.getContainerInterface().shouldCancelCurrentGesture()) || myCancel;
         boolean cleanUpConsumer = (action == ACTION_UP || action == ACTION_CANCEL || cancelGesture)
                 && mConsumer != null
                 && !mConsumer.getActiveConsumerInHierarchy().isConsumerDetachedFromGesture();
