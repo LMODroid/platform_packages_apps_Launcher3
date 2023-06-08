3fc95404833687d4680247aacd8a1e481092347e
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2023-06-09 00:22:50 +0330

    Revert "Launcher3: Ability to toggle themed icons for all apps"
    This reverts commit c938a2bc8e2e74844d0010c385aa24fa790e0e2a.
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I0d2ff4340797026a534bc47a30be97c645e11726
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- src/com/android/launcher3/BubbleTextView.java
+++ src/com/android/launcher3/BubbleTextView.java
@@ -19,7 +19,6 @@ package com.android.launcher3;
 import static com.android.launcher3.InvariantDeviceProfile.KEY_SHOW_DESKTOP_LABELS;
 import static com.android.launcher3.InvariantDeviceProfile.KEY_SHOW_DRAWER_LABELS;
 import static com.android.launcher3.InvariantDeviceProfile.KEY_MAX_LINES;
-import static com.android.launcher3.InvariantDeviceProfile.KEY_ALLAPPS_THEMED_ICONS;
 import static com.android.launcher3.config.FeatureFlags.ENABLE_ICON_LABEL_AUTO_SCALING;
 import static com.android.launcher3.graphics.PreloadIconDrawable.newPendingIcon;
 import static com.android.launcher3.icons.BitmapInfo.FLAG_NO_BADGE;
@@ -178,7 +177,6 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
     private boolean mDisableRelayout = false;
 
     private boolean mShouldShowLabel;
-    private boolean mThemeAllAppsIcons;
     private int mMaxLines;
 
     private HandlerRunnable mIconLoadRequest;
@@ -219,7 +217,6 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
             setCompoundDrawablePadding(grid.allAppsIconDrawablePaddingPx);
             defaultIconSize = grid.allAppsIconSizePx;
             mShouldShowLabel = prefs.getBoolean(KEY_SHOW_DRAWER_LABELS, true);
-            mThemeAllAppsIcons = prefs.getBoolean(KEY_ALLAPPS_THEMED_ICONS, false);
         } else if (mDisplay == DISPLAY_FOLDER) {
             setTextSize(TypedValue.COMPLEX_UNIT_PX, grid.folderChildTextSizePx);
             setCompoundDrawablePadding(grid.folderChildDrawablePaddingPx);
@@ -388,7 +385,7 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
     @UiThread
     protected void applyIconAndLabel(ItemInfoWithIcon info) {
         boolean useTheme = mDisplay == DISPLAY_WORKSPACE || mDisplay == DISPLAY_FOLDER
-                || mDisplay == DISPLAY_TASKBAR || (mThemeAllAppsIcons && mDisplay == DISPLAY_ALL_APPS);
+                || mDisplay == DISPLAY_TASKBAR || mDisplay == DISPLAY_ALL_APPS;
         int flags = useTheme ? FLAG_THEMED : 0;
         if (mHideBadge) {
             flags |= FLAG_NO_BADGE;
