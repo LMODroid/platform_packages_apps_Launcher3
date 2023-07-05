cc295c1026bb7612d59991e4b6ac813a2562b782
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-07-06 01:10:26 +0530

    Launcher3: Switch to search widget provider for hotseat QSB
    We don't need to create our own custom QSB view, Google app already
    provides search widget which QsbContainerView automatically binds to.
    
    Change-Id: I8c1beb227b6417d606e4229bdb6222ad74dd5d73
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/qsb/QsbContainerView.java
+++ src/com/android/launcher3/qsb/QsbContainerView.java
@@ -291,7 +291,7 @@ public class QsbContainerView extends FrameLayout {
         }
 
         public boolean isQsbEnabled() {
-            return FeatureFlags.QSB_ON_FIRST_SCREEN
+            return Utilities.showQSB(getContext())
                     && !SHOULD_SHOW_FIRST_PAGE_WIDGET;
         }
 
