ff9db1a689f1b9c5de3d3fe0a926f42f3d32e07d
nift4 <nift4@protonmail.com>
2023-10-03 23:30:41 +0200

    Launcher3: fix overview scrolling scale animation
    adapt it for Live Tile support + fix landscape handling
    
    Change-Id: I1cf5d79aa6373e836d33bc7ddb15c0c5258a63d7
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -2631,6 +2631,7 @@ public abstract class AbsSwipeUpHandler<
             startInterceptingTouchesForGesture();
         }
         for (RemoteTargetHandle remoteHandle : mRemoteTargetHandles) {
+            float scrollScale = setRecentsScroll ? mRecentsView.getScrollScale(remoteHandle) : 1f;
             AnimatorControllerWithResistance playbackController =
                     remoteHandle.getPlaybackController();
             if (playbackController != null) {
@@ -2639,6 +2640,7 @@ public abstract class AbsSwipeUpHandler<
 
             if (notSwipingToHome) {
                 TaskViewSimulator taskViewSimulator = remoteHandle.getTaskViewSimulator();
+                taskViewSimulator.scrollScale.value = scrollScale;
                 if (setRecentsScroll) {
                     taskViewSimulator.setScroll(scrollOffset);
                 }
