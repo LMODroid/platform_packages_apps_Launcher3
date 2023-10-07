432c65460c6ae3af48ed028cc50f7c5ecb4a1c83
nift4 <nift4@protonmail.com>
2023-10-07 11:58:04 +0200

    Launcher3: fix very rare race condition
    Change-Id: Ifc37c3755dd53f940e09847566771fcdaa16d01a
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/qsb/QsbContainerView.java
+++ src/com/android/launcher3/qsb/QsbContainerView.java
@@ -275,8 +275,8 @@ public class QsbContainerView extends FrameLayout implements SharedPreferences.O
         LauncherPrefs.getPrefs(getContext()).registerOnSharedPreferenceChangeListener(this);
         if (isQsbEnabled()) {
             mQsbWidgetHost.startListening();
-            addView(createQsb(this));
         }
+        rebindFragment();
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(ACTION_PACKAGE_ADDED);
         intentFilter.addAction(ACTION_PACKAGE_CHANGED);
@@ -291,7 +291,6 @@ public class QsbContainerView extends FrameLayout implements SharedPreferences.O
         getContext().unregisterReceiver(mReceiver);
         LauncherPrefs.getPrefs(getContext()).unregisterOnSharedPreferenceChangeListener(this);
         mQsbWidgetHost.stopListening();
-        removeAllViews();
     }
 
     private void rebindFragment() {
