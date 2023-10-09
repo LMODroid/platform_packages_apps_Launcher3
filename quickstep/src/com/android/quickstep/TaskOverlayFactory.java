870f30514ab1716d0e743697fb37e621dc1806c6
electimon <electimon@gmail.com>
2023-10-09 20:17:48 +0800

    Launcher3: Reshuffle overview task list
    * Better UX
    
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I8e70f7e7228f9b32898ed847503cb65eb8d388c5
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -112,21 +112,21 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
     /** Note that these will be shown in order from top to bottom, if available for the task. */
     private static final TaskShortcutFactory[] MENU_OPTIONS = new TaskShortcutFactory[]{
             TaskShortcutFactory.APP_INFO,
-            TaskShortcutFactory.KILL_APP,
             TaskShortcutFactory.SPLIT_SELECT,
             TaskShortcutFactory.FLOATING,
-            TaskShortcutFactory.UNINSTALL,
-            TaskShortcutFactory.PIN,
-            TaskShortcutFactory.INSTALL,
             TaskShortcutFactory.FREE_FORM,
             DesktopSystemShortcut.Companion.createFactory(),
             ExternalDisplaySystemShortcut.Companion.createFactory(),
             AspectRatioSystemShortcut.Companion.createFactory(),
             TaskShortcutFactory.WELLBEING,
+            TaskShortcutFactory.PIN,
+            TaskShortcutFactory.INSTALL,
             TaskShortcutFactory.SAVE_APP_PAIR,
             TaskShortcutFactory.SCREENSHOT,
-            TaskShortcutFactory.MODAL,
             TaskShortcutFactory.CLOSE,
+            TaskShortcutFactory.KILL_APP,
+            TaskShortcutFactory.UNINSTALL,
+            TaskShortcutFactory.MODAL
     };
 
     /**
