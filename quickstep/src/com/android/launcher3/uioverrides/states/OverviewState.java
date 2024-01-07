37d8c6914e809264fac69c5d77d6e40bc3882104
Pranav Vashi <neobuddy89@gmail.com>
2024-01-07 15:29:45 +0530

    Launcher3: Do not tag meminfo visible if disabled
    Change-Id: I0fb6a013103f87ee8e31e8a183f3475a9c7e3f61
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/states/OverviewState.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/OverviewState.java
@@ -115,7 +115,8 @@ public class OverviewState extends LauncherState {
     @Override
     public int getVisibleElements(Launcher launcher) {
         boolean clearAll = LauncherPrefs.getPrefs(launcher).getBoolean("pref_recents_clear_all", true);
-        int elements = OVERVIEW_ACTIONS | MEMINFO | (!clearAll ? CLEAR_ALL_BUTTON : 0);
+        boolean memInfo = LauncherPrefs.getPrefs(launcher).getBoolean("pref_recents_meminfo", false);
+        int elements = OVERVIEW_ACTIONS;
         DeviceProfile dp = launcher.getDeviceProfile();
         boolean showFloatingSearch;
         if (dp.isPhone) {
@@ -128,8 +129,11 @@ public class OverviewState extends LauncherState {
         if (showFloatingSearch) {
             elements |= FLOATING_SEARCH_BAR;
         }
-        if (enableSplitContextual() && launcher.isSplitSelectionActive()) {
-            elements &= ~CLEAR_ALL_BUTTON;
+        if (!clearAll && !(enableSplitContextual() && launcher.isSplitSelectionActive())) {
+            elements |= CLEAR_ALL_BUTTON;
+        }
+        if (memInfo) {
+            elements |= MEMINFO;
         }
         return elements;
     }
