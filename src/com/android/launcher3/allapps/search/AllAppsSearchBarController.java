bbd670a0ece05cabb67aa941379946f5777cfe36
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-07-22 01:13:26 +0530

    Launcher3: allapps: Do not hide keyboard when query is empty
    Change-Id: I78f164358cb4fcd23bcb858222a88008d67b2ca4
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/launcher3/allapps/search/AllAppsSearchBarController.java
+++ src/com/android/launcher3/allapps/search/AllAppsSearchBarController.java
@@ -155,7 +155,6 @@ public class AllAppsSearchBarController
     public void reset() {
         mCallback.clearSearchResult();
         mInput.reset();
-        mInput.clearFocus();
         mQuery = null;
         mInput.removeOnFocusChangeListener(this);
     }
