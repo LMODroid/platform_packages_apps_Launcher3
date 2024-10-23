5376f048ecb5a56d196f848eebf456de8b32341c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-10-23 19:08:42 +0530

    Launcher3: Fixup qsb horizontal margin
    We need to add more margin - reduce the width, not increase it.
    
    Change-Id: Iccdf32a9e3133c96c7fc8715a6913306eab8b9e2

--- src/com/android/launcher3/Hotseat.java
+++ src/com/android/launcher3/Hotseat.java
@@ -253,8 +253,8 @@ public class Hotseat extends CellLayout implements Insettable {
                 ? dp.hotseatQsbWidth
                 : getShortcutsAndWidgets().getMeasuredWidth();
 
-        qsbWidth += (getContext().getResources().getDimensionPixelSize(
-                R.dimen.hotseat_qsb_excess_horizontal_margin)) * 2;
+        qsbWidth -= getContext().getResources().getDimensionPixelSize(
+                R.dimen.hotseat_qsb_extra_horizontal_margin) * 2;
 
         mQsb.measure(MeasureSpec.makeMeasureSpec(qsbWidth, MeasureSpec.EXACTLY),
                 MeasureSpec.makeMeasureSpec(dp.hotseatQsbHeight, MeasureSpec.EXACTLY));
