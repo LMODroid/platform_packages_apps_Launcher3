2e7fe9d9b7f76242826e32bf890632025124ab44
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-10-27 07:41:48 +0530

    Launcher3: Improve meminfo view height and margins
    - Take account of meminfo view height in overview to avoid overlapping
      in landscape mode.
    - Borrow the overview actions' bottom margin instead of hardcoding it.
    
    Change-Id: I4e5efb63ebfa0cb35bcaf2fb1008a6655e567de6

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -394,9 +394,7 @@ public class DeviceProfile {
     }
 
     // Meminfo in overview
-    public int memInfoMarginGesturePx;
-    public int memInfoMarginTransientTaskbarPx;
-    public int memInfoMarginThreeButtonPx;
+    public int memInfoHeight;
 
     /** TODO: Once we fully migrate to staged split, remove "isMultiWindowMode" */
     DeviceProfile(Context context, InvariantDeviceProfile inv, Info info,
@@ -825,12 +823,8 @@ public class DeviceProfile {
             isLeftRightSplit = isLandscape;
         }
 
-        memInfoMarginGesturePx = res.getDimensionPixelSize(
-                R.dimen.meminfo_bottom_margin_gesture);
-        memInfoMarginTransientTaskbarPx = res.getDimensionPixelSize(
-                R.dimen.meminfo_bottom_margin_transient_taskbar);
-        memInfoMarginThreeButtonPx = res.getDimensionPixelSize(
-                R.dimen.meminfo_bottom_margin_three_button);
+        memInfoHeight = Utilities.isShowMeminfo(context) ? res.getDimensionPixelSize(
+                R.dimen.meminfo_claimed_height) : 0;
 
         // Calculate all of the remaining variables.
         extraSpace = updateAvailableDimensions(context);
@@ -2183,7 +2177,7 @@ public class DeviceProfile {
         int overviewActionsSpace = isTablet && Flags.enableGridOnlyOverview()
                 ? 0
                 : (overviewActionsTopMarginPx + overviewActionsHeight);
-        return overviewActionsSpace + getOverviewActionsClaimedSpaceBelow();
+        return overviewActionsSpace + memInfoHeight + getOverviewActionsClaimedSpaceBelow();
     }
 
     /**
