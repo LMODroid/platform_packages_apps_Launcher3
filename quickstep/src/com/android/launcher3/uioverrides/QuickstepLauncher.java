0f806e86c429646fa4638298ff388ddf4479e89e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-04 08:52:33 +0800

    Launcher3: Integrate LMOFreeform to system shortcut
    @ neobuddy89: Rename this to FREE_FORM to FLOATING following TaskShortcutFactory.
    @ adithya2306: De-duplicate code, fix launching profile apps, skip non-resizeable apps.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I8a5629e82f10e2c4ef08e23465e97fd010769e72

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -46,6 +46,7 @@ import static com.android.launcher3.model.data.ItemInfo.NO_MATCHING_ID;
 import static com.android.launcher3.popup.QuickstepSystemShortcut.getSplitSelectShortcutByPosition;
 import static com.android.launcher3.popup.SystemShortcut.APP_INFO;
 import static com.android.launcher3.popup.SystemShortcut.DONT_SUGGEST_APP;
+import static com.android.launcher3.popup.SystemShortcut.FLOATING;
 import static com.android.launcher3.popup.SystemShortcut.INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.PRIVATE_PROFILE_INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.UNINSTALL_APP;
@@ -479,6 +480,7 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer 
         List<SystemShortcut.Factory> shortcuts = new ArrayList(Arrays.asList(
                 APP_INFO, WellbeingModel.SHORTCUT_FACTORY, mHotseatPredictionController));
         shortcuts.addAll(getSplitShortcuts());
+        shortcuts.add(FLOATING);
         shortcuts.add(WIDGETS);
         shortcuts.add(INSTALL);
         if (Flags.enablePrivateSpaceInstallShortcut()) {
