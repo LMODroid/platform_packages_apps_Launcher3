afd8474dd120349235c02b04a7e8f97fcf7ee94e
Michael Bestas <mkbestas@lineageos.org>
2025-03-25 15:05:03 +0200

    Launcher3: Fix taskbar/3 button navigation SUW height
    Taskbar height was broken on tablet in the past few QPRs, now it also
    broke on phones since they use taskar.
    Explicitly setting gesture navigation doesn't fix anything in SUW, so
    just delete all the custom handling and everything works as expected.
    
    Change-Id: I9ba2e320c02981fe75faa7189ca0707208a34814

--- quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
@@ -789,9 +789,6 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
 
     public void onConfigurationChanged(@Config int configChanges) {
         mControllers.onConfigurationChanged(configChanges);
-        if (!mIsUserSetupComplete) {
-            setTaskbarWindowSize(getSetupWindowSize());
-        }
     }
 
     public boolean isThreeButtonNav() {
@@ -1336,10 +1333,6 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
                     resources.getDimensionPixelSize(R.dimen.taskbar_stashed_size);
         }
 
-        if (!isUserSetupComplete()) {
-            return getSetupWindowSize();
-        }
-
         int bubbleBarTop = mControllers.bubbleControllers.map(bubbleControllers ->
                 bubbleControllers.bubbleBarViewController.getBubbleBarWithFlyoutMaximumHeight()
         ).orElse(0);
@@ -1372,10 +1365,6 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
                 + getCornerRadius()
                 + extraHeightForTaskbarTooltips;
         return Math.max(taskbarWindowSize, bubbleBarTop);
-    }
-
-    public int getSetupWindowSize() {
-        return getResources().getDimensionPixelSize(R.dimen.taskbar_suw_frame);
     }
 
     public DeviceProfile getTransientTaskbarDeviceProfile() {
