6d48194457b1b0c5d49f8aee254d61291b7f9abb
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-10 06:42:33 +0800

    Launcher3: SysUiScrim: fix memory leak
    Change-Id: Iae0efd6d7c1bc0cdab32319dc9f5f8f46d82c659
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/launcher3/graphics/SysUiScrim.java
+++ src/com/android/launcher3/graphics/SysUiScrim.java
@@ -108,11 +108,7 @@ public class SysUiScrim implements View.OnAttachStateChangeListener,
         SharedPreferences prefs = LauncherPrefs.getPrefs(view.getContext());
         mHideSysUiScrim = !prefs.getBoolean(KEY_SHOW_TOP_SHADOW, true);
         createMaskBitmaps();
-
-        if (!mHideSysUiScrim) {
-            view.addOnAttachStateChangeListener(this);
-        }
-        prefs.registerOnSharedPreferenceChangeListener(this);
+        view.addOnAttachStateChangeListener(this);
     }
 
     /**
@@ -174,11 +170,13 @@ public class SysUiScrim implements View.OnAttachStateChangeListener,
     @Override
     public void onViewAttachedToWindow(View view) {
         ScreenOnTracker.INSTANCE.get(mContainer.asContext()).addListener(mScreenOnListener);
+        LauncherPrefs.getPrefs(view.getContext()).registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     public void onViewDetachedFromWindow(View view) {
         ScreenOnTracker.INSTANCE.get(mContainer.asContext()).removeListener(mScreenOnListener);
+        LauncherPrefs.getPrefs(view.getContext()).unregisterOnSharedPreferenceChangeListener(this);
     }
 
     @Override
