crdroidandroid/android_packages_apps_Launcher3

Launcher3: Restart when FORCE_SHOW_NAVBAR changes
Change-Id: I96d32d36d4182d9ecb3bc0d7036caaf540bd7b58

quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -133,6 +133,9 @@ public class TaskbarManager {
     public static final Uri GESTURE_NAVBAR_HEIGHT_MODE = Settings.System.getUriFor(
             Settings.System.GESTURE_NAVBAR_HEIGHT_MODE);
 
+    public static final Uri FORCE_SHOW_NAVBAR = LineageSettings.System.getUriFor(
+            LineageSettings.System.FORCE_SHOW_NAVBAR);
+
     private final Context mWindowContext;
     private final @Nullable Context mNavigationBarPanelContext;
     private WindowManager mWindowManager;
@@ -280,6 +283,8 @@ public class TaskbarManager {
                 .register(GESTURE_NAVBAR_LENGTH_MODE, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)
                 .register(GESTURE_NAVBAR_HEIGHT_MODE, mOnTaskBarChangeListener);
+        SettingsCache.INSTANCE.get(mWindowContext)
+                .register(FORCE_SHOW_NAVBAR, mOnTaskBarChangeListener);
         Log.d(TASKBAR_NOT_DESTROYED_TAG, "registering component callbacks from constructor.");
         mWindowContext.registerComponentCallbacks(mDefaultComponentCallbacks);
         mShutdownReceiver.register(mWindowContext, Intent.ACTION_SHUTDOWN);
@@ -762,6 +767,8 @@ public class TaskbarManager {
                 .unregister(GESTURE_NAVBAR_LENGTH_MODE, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mWindowContext)
                 .unregister(GESTURE_NAVBAR_HEIGHT_MODE, mOnTaskBarChangeListener);
+        SettingsCache.INSTANCE.get(mWindowContext)
+                .unregister(FORCE_SHOW_NAVBAR, mOnTaskBarChangeListener);
         Log.d(TASKBAR_NOT_DESTROYED_TAG, "unregistering component callbacks from destroy().");
         mWindowContext.unregisterComponentCallbacks(mDefaultComponentCallbacks);
         mShutdownReceiver.unregisterReceiverSafely(mWindowContext);

LMODroid/platform_packages_apps_Launcher3

quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -153,6 +153,9 @@ public class TaskbarManager implements DisplayDecorationListener {
     public static final Uri NAVIGATION_BAR_HINT = Settings.System.getUriFor(
             LMOSettings.System.NAVIGATION_BAR_HINT);
 
+    public static final Uri FORCE_SHOW_NAVBAR = Settings.System.getUriFor(
+            LMOSettings.System.FORCE_SHOW_NAVBAR);
+
     private final Context mBaseContext;
     private final int mPrimaryDisplayId;
     private final TaskbarNavButtonCallbacks mNavCallbacks;
@@ -479,6 +482,8 @@ public class TaskbarManager implements DisplayDecorationListener {
                 .register(ENABLE_TASKBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .register(NAVIGATION_BAR_HINT, mOnTaskBarChangeListener);
+        SettingsCache.INSTANCE.get(mPrimaryWindowContext)
+                .register(FORCE_SHOW_NAVBAR, mOnTaskBarChangeListener);
         SystemDecorationChangeObserver.getINSTANCE().get(mPrimaryWindowContext)
                 .registerDisplayDecorationListener(this);
         mShutdownReceiver =
@@ -1138,6 +1143,8 @@ public class TaskbarManager implements DisplayDecorationListener {
                 .unregister(ENABLE_TASKBAR, mOnTaskBarChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .unregister(NAVIGATION_BAR_HINT, mOnTaskBarChangeListener);
+        SettingsCache.INSTANCE.get(mPrimaryWindowContext)
+                .unregister(FORCE_SHOW_NAVBAR, mOnTaskBarChangeListener);
         SystemDecorationChangeObserver.getINSTANCE().get(mPrimaryWindowContext)
                 .unregisterDisplayDecorationListener(this);
         debugPrimaryTaskbar("destroy: unregistering component callbacks");