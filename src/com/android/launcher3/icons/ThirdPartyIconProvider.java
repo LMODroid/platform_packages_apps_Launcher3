71af382156be00b595abceb1083aa5f91443403f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-08-14 22:17:15 +0530

    Launcher3: Fix icon pack application in recents
    Recents uses ActivityInfo instead of LauncherActivityInfo so we need
    to override the method with that signature as well.
    
    Change-Id: Ibea24de03ba802222b678ebec83c9b579d93f187

--- src/com/android/launcher3/icons/ThirdPartyIconProvider.java
+++ src/com/android/launcher3/icons/ThirdPartyIconProvider.java
@@ -1,12 +1,12 @@
 package com.android.launcher3.icons;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.content.pm.LauncherActivityInfo;
 import android.graphics.drawable.Drawable;
+import android.os.UserHandle;
 
-import android.annotation.SuppressLint;
 import com.android.launcher3.util.ComponentKey;
-
 import com.android.launcher3.icons.pack.IconResolver;
 
 import static com.android.launcher3.icons.BaseIconFactory.CONFIG_HINT_NO_WRAP;
@@ -20,7 +20,6 @@ public class ThirdPartyIconProvider extends LauncherIconProvider {
         mContext = context;
     }
 
-    @SuppressLint("WrongConstant")
     @Override
     public Drawable getIcon(LauncherActivityInfo launcherActivityInfo, int iconDpi, String themedIconPack) {
         ComponentKey key = new ComponentKey(
@@ -28,6 +27,21 @@ public class ThirdPartyIconProvider extends LauncherIconProvider {
 
         IconResolver.DefaultDrawableProvider fallback =
                 () -> super.getIcon(launcherActivityInfo, iconDpi, themedIconPack);
+        Drawable icon = ThirdPartyIconUtils.getByKey(mContext, key, iconDpi, fallback);
+
+        if (icon == null) {
+            return fallback.get();
+        }
+        icon.setChangingConfigurations(icon.getChangingConfigurations() | CONFIG_HINT_NO_WRAP);
+        return icon;
+    }
+
+    @Override
+    public Drawable getIcon(ActivityInfo activityInfo, int iconDpi, String themedIconPack) {
+        ComponentKey key = new ComponentKey(activityInfo.getComponentName(), UserHandle.CURRENT);
+
+        IconResolver.DefaultDrawableProvider fallback =
+                () -> super.getIcon(activityInfo, iconDpi, themedIconPack);
         Drawable icon = ThirdPartyIconUtils.getByKey(mContext, key, iconDpi, fallback);
 
         if (icon == null) {
