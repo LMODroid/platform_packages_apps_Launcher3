0da31fadbe7dc8d54d18d6cf59615c6993733773
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-08-29 12:45:56 +0530

    Launcher3: Update floating window drawable
    Change-Id: Ie360f3cc0d04919fd216d3061adb240603e46684

--- quickstep/src/com/android/quickstep/TaskShortcutFactory.java
+++ quickstep/src/com/android/quickstep/TaskShortcutFactory.java
@@ -299,7 +299,7 @@ public interface TaskShortcutFactory {
 
         public FloatingSystemShortcut(RecentsViewContainer container, TaskContainer taskContainer) {
             // TODO new icon?
-            super(R.drawable.float_portrait_2_24px, R.string.floating_window,
+            super(R.drawable.picture_in_picture_mobile_24px, R.string.floating_window,
                     container, taskContainer.getItemInfo(), taskContainer.getTaskView());
             mTaskView = taskContainer.getTaskView();
         }
